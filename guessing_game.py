e10d65b98cfab6e9219253343650cc98b8dae45b
muddanadharshini-sudo <muddanadharshini@gmail.com>
2026-07-05 14:03:55 +0530

    Completed Task-02: Guessing Game

---
+++ guessing_game.py
@@ -0,0 +1,16 @@
+import random
+secret_number = random.randint(1, 100)
+attempts = 0
+print("=== Number Guessing Game ===")
+print("Guess a number between 1 and 100")
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("\nCongratulations! You guessed the correct number.")
+        print("Number of attempts:", attempts)
+        break
